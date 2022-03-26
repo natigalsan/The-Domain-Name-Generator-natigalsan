e1cf17b0d18119fe9dea453d4e3b93ad4466d96c
natigalsan <natividadgalsan@gmail.com>
2022-03-26 10:51:26 +0000

    Domain name generator-natigalsan

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dominios = [".com", ".net", ".us"];
+//-----------------------------------según enunciado usar bucles anidados----------------------------------------
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let x = 0; x < noun.length; x++) {
+        for (let y = 0; y < dominios.length; y++) {
+          console.log(pronoun[i] + adj[j] + noun[x] + dominios[y]);
+        }
+      }
+    }
+  }
 };
